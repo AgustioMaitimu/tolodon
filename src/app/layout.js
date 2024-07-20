73e4eb965a8f873fd3869c5b642194f9276bd0c4
AgustioMaitimu <agustiomaitimu8@gmail.com>
2024-07-21 04:54:08 +0700

    fix: changed wrong site title

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Footer from '@/components/Footer';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <title>Agustio | Personal Portfolio</title>
+      <title>Tolodon | Conceptual E-Commerce Experience</title>
       <body className="flex flex-col items-center">
         <Header />
         {children}
